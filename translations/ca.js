067da0e1e7961a616e8356799bfb7e9256fd350e
ihorpopach <ihor.p.rivoagency@gmail.com>
2020-09-30 14:53:24 +0300

    Updates

---
+++ translations/ca.js
@@ -0,0 +1,1 @@
+(function(d){	const l = d['ca'] = d['ca'] || {};	l.dictionary=Object.assign(		l.dictionary||{},		{"Block quote":"Cita de bloc",Bold:"Negreta",Cancel:"Cancel·lar","Cannot upload file:":"No es pot pujar l'arxiu:",Italic:"Cursiva",Save:"Desar"}	);l.getPluralForm=function(n){return (n != 1);;};})(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
